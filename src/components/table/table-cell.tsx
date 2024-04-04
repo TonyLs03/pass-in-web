883c9bfd10c6b0b8fa23eb210b831a28158cd1c6
TonyLs03 <azulynot@gmail.com>
2024-04-04 16:03:28 -0300

    componentes-parte-2
    deixando aplicação mais dinâmica, ajustando tabela e componentes novos

--- src/components/table/table-cell.tsx
+++ src/components/table/table-cell.tsx
@@ -1,12 +1,10 @@
 import { ComponentProps } from "react";
 import { twMerge } from 'tailwind-merge'
 
-interface TableCellProps extends ComponentProps<'th'> {
-
-}
+interface TableCellProps extends ComponentProps<'th'> {}
 
 export function TableCell(props: TableCellProps) {
     return(
-        <th {...props} className={twMerge("py-3 px-4 text-sm text-zinc-300", props.className)} />
+        <th {...props} className={twMerge("py-3 px-4 text-sm text-zinc-300 text-left", props.className)} />
     )
 }
